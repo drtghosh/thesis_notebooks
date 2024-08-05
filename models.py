5befd8520f6f3b735b7b04b26024d5d7a44719b9
Debabrata Ghosh <rijugh@gmail.com>
2024-08-05 16:44:48 +0200

    covariance network and residuals

--- models.py
+++ models.py
@@ -1,6 +1,7 @@
 import torch
 import torch.nn as nn
 
+
 class MLPBlock(nn.Module):
     """
     Class of Multi-Layer Perceptron which doesn't grow (the number of hidden nodes in each layer is same)
@@ -9,15 +10,15 @@ class MLPBlock(nn.Module):
      - num_layers: number of internal layers in the MLP
      - in_dim: input dimension of data
      - out_dim: dimension of the resulting output
-     - nonlin_layer: non-linearity added to the network layers (e.g. ReLU, softmax, ...)
+     - nonlinear_layer: non-linearity added to the network layers (e.g. ReLU, softmax, ...)
     """
-    def __init__(self, h_nodes=64, num_layers=2, in_dim=1, out_dim=1, nonlin_layer=nn.ReLU()):
+    def __init__(self, h_nodes=64, num_layers=2, in_dim=1, out_dim=1, nonlinear_layer=nn.ReLU()):
         super(MLPBlock, self).__init__()
         self.h_nodes = h_nodes
         self.num_layers = num_layers
         self.in_dim = in_dim
         self.out_dim = out_dim
-        self.nonlin_layer = nonlin_layer
+        self.nonlinear_layer = nonlinear_layer
 
         # creating the linear (fully connected) layers
         self.layers = nn.ModuleList()
@@ -29,7 +30,6 @@ class MLPBlock(nn.Module):
         # initialize the weights
         for i in range(len(self.layers)):
             nn.init.xavier_uniform_(self.layers[i].weight)
-            # nn.init.zeros_(self.layers[i].bias)
 
     def forward(self, x):
         # pass input through first layer
@@ -42,6 +42,7 @@ class MLPBlock(nn.Module):
 
         return out
 
+
 class MLPGrow(nn.Module):
     """
     Class of Multi-Layer Perceptron which grows by a multiplier
@@ -52,16 +53,16 @@ class MLPGrow(nn.Module):
      - multiplier: the factor by which the number of hidden nodes grows
      - in_dim: input dimension of data
      - out_dim: dimension of the resulting output
-     - nonlin_layer: non-linearity added to the network layers (e.g. ReLU, softmax, ...)
+     - nonlinear_layer: non-linearity added to the network layers (e.g. ReLU, softmax, ...)
     """
-    def __init__(self, h_nodes=64, num_layers=2, multiplier=2, in_dim=1, out_dim=1, nonlin_layer=nn.ReLU()):
+    def __init__(self, h_nodes=64, num_layers=2, multiplier=2, in_dim=1, out_dim=1, nonlinear_layer=nn.ReLU()):
         super(MLPGrow, self).__init__()
         self.h_nodes = h_nodes
         self.num_layers = num_layers
         self.multiplier = multiplier
         self.in_dim = in_dim
         self.out_dim = out_dim
-        self.nonlin_layer = nonlin_layer
+        self.nonlinear_layer = nonlinear_layer
 
         # creating the linear (fully connected) layers
         self.layers = nn.ModuleList()
@@ -75,7 +76,6 @@ class MLPGrow(nn.Module):
         # initialize the weights
         for i in range(len(self.layers)):
             nn.init.xavier_uniform_(self.layers[i].weight)
-            # nn.init.zeros_(self.layers[i].bias)
 
     def forward(self, x):
         # pass input through first layer
@@ -88,7 +88,8 @@ class MLPGrow(nn.Module):
 
         return out
 
-class covNet(nn.Module):
+
+class CovNet(nn.Module):
     """
     Network to compute the covariance between two points
     Initialization Parameters:
@@ -96,15 +97,15 @@ class covNet(nn.Module):
      - num_layers: number of internal layers in the MLP
      - in_dim: input dimension of data
      - out_dim: dimension of the resulting output
-     - nonlin_layer: non-linearity added to the network layers (e.g. ReLU, softmax, ...)
+     - nonlinear_layer: non-linearity added to the network layers (e.g. ReLU, softmax, ...)
     """
-    def __init__(self, h_nodes=64, num_layers=2, in_dim=1, out_dim=1, nonlin_layer=nn.ReLU()):
-        super(covNet, self).__init__()
+    def __init__(self, h_nodes=64, num_layers=2, in_dim=1, out_dim=1, nonlinear_layer=nn.ReLU()):
+        super(CovNet, self).__init__()
         self.h_nodes = h_nodes
         self.num_layers = num_layers
         self.in_dim = in_dim
         self.out_dim = out_dim
-        self.m = nonlin_layer
+        self.nonlinear_layer = nonlinear_layer
 
         # creating the linear (fully connected) layers
         self.layers = nn.ModuleList()
@@ -116,14 +117,13 @@ class covNet(nn.Module):
         # initialize the weights
         for i in range(len(self.layers)):
             nn.init.xavier_uniform_(self.layers[i].weight)
-            # nn.init.zeros_(self.layers[i].bias)
 
     def individual_pass(self, x):
         # pass input through first layer
-        x = self.nonlin_layer(self.layers[0](x))
+        x = self.nonlinear_layer(self.layers[0](x))
         # pass through internal layers
         for i in range(1, self.num_layers):
-            x = self.nonlin_layer(self.layers[i](x))
+            x = self.nonlinear_layer(self.layers[i](x))
         # pass through last layer to get output (no activation on the last layer)
         x = self.layers[self.num_layers + 1](x)
         # enforce positivity on the output
@@ -132,60 +132,90 @@ class covNet(nn.Module):
         return out
 
     def forward(self, x):
-        out1 = self.individual_pass(x)
-        # Swap the first dim dimensions with the second dim dimensions
-        dim = x.shape[1]//2
-        first_dim_columns = x[:, :dim]
-        second_dim_columns = x[:, dim:]
-        # Swap the columns and concatenate the tensors
-        swapped_input = torch.cat((second_dim_columns, first_dim_columns), dim=1)
-        out2 = self.individual_pass(swapped_input)
+        # compute covariance for original input pair
+        cov1 = self.individual_pass(x)
+        # collect the dimension of individual input (of pair)
+        individual_dim = self.in_dim//2
+        # swap the inputs and concatenate the tensors to compute the covariance
+        swapped_pair = torch.cat((x[:, :individual_dim], x[:, individual_dim:2*individual_dim]), dim=1)
+        cov2 = self.individual_pass(swapped_pair)
 
-        # Swap the columns back
-        return out1 + out2
+        # take an average to ensure symmetry
+        return cov1 + cov2
 
 
-class deep_ritz_block(nn.Module):
-    def __init__(self, k=64, device=None, m=torch.nn.ReLU()):
-        super(deep_ritz_block, self).__init__()
-        self.k = k
-        self.device = device
-        self.m = m
-        self.first_layer = nn.Linear(k, k).to(device)
-        nn.init.xavier_uniform_(self.first_layer.weight)
-        self.second_layer = nn.Linear(k, k).to(device)
-        nn.init.xavier_uniform_(self.second_layer.weight)
+class ResBlock(nn.Module):
+    """
+        Residual block for the deep residual network
+        Initialization Parameters:
+         - h_nodes: number of hidden nodes for each layer
+         - block_length: number of layers after which residual connection is added
+         - nonlinear_layer: non-linearity added to the network layers (e.g. ReLU, softmax, ...)
+        """
+    def __init__(self, h_nodes=64, block_length=2, nonlinear_layer=torch.nn.ReLU()):
+        super(ResBlock, self).__init__()
+        self.h_nodes = h_nodes
+        self.block_length = block_length
+        self.nonlinear_layer = nonlinear_layer
 
-    def forward(self, inputs):
-        # out = inputs.to(self.device)
-        out = self.first_layer(inputs)
-        out = self.m(out)
-        out = self.second_layer(out)
-        out = self.m(out)
-        out = out + inputs
+        # creating the linear (fully connected) layers
+        self.layers = nn.ModuleList()
+        for i in range(block_length):
+            self.layers.append(nn.Linear(h_nodes, h_nodes))
+
+        # initialize the weights
+        for i in range(block_length):
+            nn.init.xavier_uniform_(self.layers[i].weight)
+
+    def forward(self, x):
+        # store input
+        x_in = x
+        # pass through the layers
+        for i in range(self.block_length):
+            x = self.nonlinear_layer(self.layers[i](x))
+        # add residual
+        out = x_in + x
         return out
 
 
-class deep_ritz_network(nn.Module):
-    def __init__(self, input_dim=2, output_dim=1, k=64, device=None, m=torch.nn.ReLU(), num_blocks=2):
-        super(deep_ritz_network, self).__init__()
-        self.k = k
-        self.device = device
-        self.m = m
+class ResDeepBlock(nn.Module):
+    """
+        Deep network with residual connections
+        Initialization Parameters:
+         - h_nodes: number of hidden nodes for each layer
+         - num_blocks: number of residual blocks in the network
+         - block_length: length (number of layers) of each residual connection
+         - in_dim: input dimension of data
+         - out_dim: dimension of the resulting output
+         - nonlinear_layer: non-linearity added to the network layers (e.g. ReLU, softmax, ...)
+        """
+    def __init__(self, h_nodes=64, num_blocks=2, block_length=2, in_dim=1, out_dim=1, nonlinear_layer=nn.ReLU()):
+        super(ResDeepBlock, self).__init__()
+        self.h_nodes = h_nodes
         self.num_blocks = num_blocks
-        self.input_dim = input_dim
-        self.first_layer = nn.Linear(input_dim, k).to(device)
-        self.blocks = nn.ModuleList()
-        for i in range(num_blocks):
-            self.blocks.append(deep_ritz_block(k=k, device=device, m=m))
-        self.last_layer = nn.Linear(k, output_dim).to(device)
-        nn.init.xavier_uniform_(self.first_layer.weight)
-        nn.init.xavier_uniform_(self.last_layer.weight)
+        self.block_length = block_length
+        self.in_dim = in_dim
+        self.out_dim = out_dim
+        self.nonlinear_layer = nonlinear_layer
 
-    def forward(self, inputs):
-        out = self.first_layer(inputs)
-        # out = self.m(out)
-        for i in range(self.num_blocks):
-            out = self.blocks[i](out)
-        out = self.last_layer(out)
+        # creating the layers with internal residual blocks
+        self.layers = nn.ModuleList()
+        self.layers.append(nn.Linear(in_dim, h_nodes))
+        for i in range(num_blocks):
+            self.layers.append(ResBlock(h_nodes=h_nodes, block_length=block_length, nonlinear_layer=nonlinear_layer))
+        self.layers.append(nn.Linear(h_nodes, out_dim))
+
+        # initialize the weights of non-residual layers
+        nn.init.xavier_uniform_(self.layers[0].weight)
+        nn.init.xavier_uniform_(self.layers[len(self.layers)-1].weight)
+
+    def forward(self, x):
+        # pass input through first layer
+        x = self.nonlin_layer(self.layers[0](x))
+        # pass through internal residual blocks
+        for i in range(1, len(self.layers)-1):
+            x = self.layers[i](x)
+        # pass through last layer to get output (no activation on the last layer)
+        out = self.layers[len(self.layers)-1](x)
+        
         return out
