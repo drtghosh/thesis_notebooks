a467f184b19622dffe9f5799c26924e02d941b40
Debabrata Ghosh <rijugh@gmail.com>
2024-08-06 16:25:42 +0200

    deep kernel suq script

--- dklsuq.py
+++ dklsuq.py
@@ -1,11 +1,11 @@
 # import numpy as np
 import torch
-import torch.nn as nn
+# import torch.nn as nn
 # import time
 # import sys
 # import os
 # import gpytoolbox
-from .models import CovNet, ResDeepBlock
+from .models import MLPGrow
 # from itertools import cycle
 # import matplotlib.pyplot as plt
 # import pickle
@@ -13,7 +13,7 @@ from .models import CovNet, ResDeepBlock
 # import multiprocessing
 
 
-class NeuralSUQ:
+class DeepKernelSUQ:
     """
     Class of Neural Shape Uncertainty Quantification conditioned on a given incomplete point cloud
     by minimizing the -ve log likelihood of the complete point cloud data estimate from the posterior
@@ -25,10 +25,9 @@ class NeuralSUQ:
      - latent_dim: dimension of the generated latent code for partial point cloud
      - cov_layers: number of internal layers for the covariance network
      - hidden_nodes: number of hidden nodes in the covariance network inner layers
-     - add_residual: boolean to decide whether to use residual blocks or not
      - device: use 'cuda' if available
     """
-    def __init__(self, space_dim=2, point_cloud=None, partial_cloud=None, latent_dim=1024, cov_layers=5, hidden_nodes=256, add_residual=False, device=None):
+    def __init__(self, space_dim=2, point_cloud=None, partial_cloud=None, latent_dim=1024, cov_layers=5, hidden_nodes=64, device=None):
         self.space_dim = space_dim
         self.latent_dim = latent_dim
         self.cov_layers = cov_layers
@@ -42,10 +41,7 @@ class NeuralSUQ:
             self.space_dim = space_dim
 
         # initialize the covariance network
-        if add_residual:
-            self.cov_network = nn.Sequential(ResDeepBlock(h_nodes=hidden_nodes, in_dim=(2*self.space_dim + self.latent_dim), out_dim=1, nonlinear_layer=torch.sin), nn.Softplus())
-        else:
-            self.cov_network = CovNet(h_nodes=hidden_nodes, num_layers=cov_layers, in_dim=(2*self.space_dim + self.latent_dim), out_dim=1, nonlinear_layer=torch.sin)
+        self.cov_network = MLPGrow(h_nodes=hidden_nodes, num_layers=cov_layers, in_dim=(self.space_dim + self.latent_dim), out_dim=1, nonlinear_layer=torch.sin)
         self.cov_network.to(device)
 
     def set_data(self, point_cloud, partial_cloud):
