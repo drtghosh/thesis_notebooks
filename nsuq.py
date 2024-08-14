fe4753075ebbc02a178e734af865ea40ce4aaf41
Debabrata Ghosh <rijugh@gmail.com>
2024-08-14 16:52:21 +0200

    partial point f values

--- nsuq.py
+++ nsuq.py
@@ -22,19 +22,21 @@ class NeuralSUQ:
      - space_dim: dimension of the input point space
      - point_cloud: complete point cloud dataset
      - partial_cloud: incomplete point cloud dataset
+     - partial_value: distance value of the observed partial points
      - latent_dim: dimension of the generated latent code for partial point cloud
      - cov_layers: number of internal layers for the covariance network
      - hidden_nodes: number of hidden nodes in the covariance network inner layers
      - add_residual: boolean to decide whether to use residual blocks or not
      - device: use 'cuda' if available
     """
-    def __init__(self, space_dim=2, point_cloud=None, partial_cloud=None, latent_dim=1024, cov_layers=5, hidden_nodes=256, add_residual=False, device=None):
+    def __init__(self, space_dim=2, point_cloud=None, partial_cloud=None, partial_value=None, latent_dim=1024, cov_layers=5, hidden_nodes=256, add_residual=False, device=None):
         self.space_dim = space_dim
         self.latent_dim = latent_dim
         self.cov_layers = cov_layers
         self.device = device
         self.point_cloud = point_cloud
         self.partial_cloud = partial_cloud
+        self.partial_value = partial_value
 
         # set point cloud data or assert that one of point cloud data and space dim is specified
         if point_cloud is not None:
@@ -54,17 +56,23 @@ class NeuralSUQ:
             self.cov_network = CovNet(h_nodes=hidden_nodes, num_layers=cov_layers, in_dim=(2*self.space_dim + self.latent_dim), out_dim=1, nonlinear_layer=torch.sin)
         self.cov_network.to(device)
 
-    def set_data(self, point_cloud, partial_cloud, partial_noise):
+    def set_data(self, point_cloud, partial_cloud, partial_value=None, with_noise=False):
         assert point_cloud.shape[0] == partial_cloud.shape[0]
         assert point_cloud.shape[2] == partial_cloud.shape[2]
         assert point_cloud.shape[1] >= partial_cloud.shape[1]
         self.point_cloud = point_cloud
         self.space_dim = point_cloud.size(-1)
         self.partial_cloud = partial_cloud
-        self.partial_noise = partial_noise
-        self.fpc_copy = torch.tensor(point_cloud, dtype=torch.float32, device=self.device)
-        self.ppc_copy = torch.tensor(partial_cloud, dtype=torch.float32, device=self.device)
-        self.fpc_repeated = torch.cat((self.fpc_copy, self.fpc_copy), dim=1).to(self.device)
+        if partial_value is not None:
+            self.partial_value = partial_value
+        else:
+            if with_noise:
+                self.partial_value = 0.01*torch.randn(self.partial_cloud.size()[:-1])
+            else:
+                self.partial_value = torch.zeros(self.partial_cloud.size()[:-1])
+        # self.fpc_copy = torch.tensor(point_cloud, dtype=torch.float32, device=self.device)
+        # self.ppc_copy = torch.tensor(partial_cloud, dtype=torch.float32, device=self.device)
+        # self.fpc_repeated = torch.cat((self.fpc_copy, self.fpc_copy), dim=1).to(self.device)
 
     def get_posterior(self, x):
         full, partial = x[:, :self.point_cloud.shape[1], :], x[:, self.point_cloud.shape[1]:, :]
@@ -91,11 +99,10 @@ class NeuralSUQ:
             kernel_ff = cov_matrix[:self.point_cloud.shape[1], :self.point_cloud.shape[1]]
             kernel_pf = cov_matrix[self.point_cloud.shape[1]:, :self.point_cloud.shape[1]]
             kernel_pp = cov_matrix[self.point_cloud.shape[1]:, self.point_cloud.shape[1]:]
-            posterior_mean = kernel_pf.T @ torch.inverse(kernel_pp) @ self.partial_noise
+            posterior_mean = kernel_pf.T @ torch.inverse(kernel_pp) @ self.partial_value
             posterior_var = kernel_ff - kernel_pf.T @ torch.inverse(kernel_pp) @ kernel_pf
             posterior_nlls[i] = -torch.distributions.MultivariateNormal(posterior_mean, posterior_var).log_prob(full[i])
 
         return posterior_nlls
 
-    def train(self, num_epochs = 20000, print_every = 1, save_path = None):
-
+    # def train(self, num_epochs = 20000, print_every = 1, save_path = None):
