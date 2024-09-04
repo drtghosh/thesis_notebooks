bee9f55eeaecb2d2b760d24cbb0e01f3153ad3c3
Debabrata Ghosh <rijugh@gmail.com>
2024-09-04 17:46:57 +0200

    not working on cuda

--- dklsuq.py
+++ dklsuq.py
@@ -122,12 +122,13 @@ class DeepKernelSUQ:
         for i in range(bs):
             # print(f'cloud {i}')
             mapped_cloud = self.cov_network(encoded_x[i])
-            covar_module = gpytorch.kernels.ScaleKernel(gpytorch.kernels.RBFKernel(ard_num_dims=mapped_cloud.size(1)))
-            cov_matrix = covar_module(mapped_cloud).evaluate_kernel().to_dense()
+            covar_module = gpytorch.kernels.ScaleKernel(
+                gpytorch.kernels.RBFKernel(ard_num_dims=mapped_cloud.size(1))).to(self.device)
+            cov_matrix = covar_module(mapped_cloud).evaluate_kernel().to_dense().to(self.device)
             kernel_ff = cov_matrix[:self.point_cloud.shape[1], :self.point_cloud.shape[1]]
             kernel_pf = cov_matrix[self.point_cloud.shape[1]:, :self.point_cloud.shape[1]]
             kernel_pp = cov_matrix[self.point_cloud.shape[1]:, self.point_cloud.shape[1]:]
-            additional_noise = self.noise_var * torch.eye(self.partial_cloud.shape[1])
+            additional_noise = self.noise_var * torch.eye(self.partial_cloud.shape[1]).to(self.device)
             kernel_with_noise = (kernel_pp + additional_noise).to(self.device)
             posterior_mean = kernel_pf.T @ torch.linalg.inv(kernel_with_noise) @ y[i]
             posterior_var = kernel_ff - kernel_pf.T @ torch.linalg.inv(kernel_with_noise) @ kernel_pf
@@ -152,12 +153,13 @@ class DeepKernelSUQ:
         for i in range(bs):
             # print(f'cloud {i}')
             mapped_cloud = self.cov_network(encoded_x[i])
-            covar_module = gpytorch.kernels.ScaleKernel(gpytorch.kernels.RBFKernel(ard_num_dims=mapped_cloud.size(1)))
-            cov_matrix = covar_module(mapped_cloud).evaluate_kernel().to_dense()
+            covar_module = gpytorch.kernels.ScaleKernel(
+                gpytorch.kernels.RBFKernel(ard_num_dims=mapped_cloud.size(1))).to(self.device)
+            cov_matrix = covar_module(mapped_cloud).evaluate_kernel().to_dense().to(self.device)
             kernel_ff = cov_matrix[:self.point_cloud.shape[1], :self.point_cloud.shape[1]]
             kernel_pf = cov_matrix[self.point_cloud.shape[1]:, :self.point_cloud.shape[1]]
             kernel_pp = cov_matrix[self.point_cloud.shape[1]:, self.point_cloud.shape[1]:]
-            additional_noise = self.noise_var * torch.eye(self.partial_cloud.shape[1])
+            additional_noise = self.noise_var * torch.eye(self.partial_cloud.shape[1]).to(self.device)
             kernel_with_noise = (kernel_pp + additional_noise).to(self.device)
             posterior_mean = kernel_pf.T @ torch.linalg.inv(kernel_with_noise) @ y[i]
             posterior_var = kernel_ff - kernel_pf.T @ torch.linalg.inv(kernel_with_noise) @ kernel_pf
@@ -169,7 +171,7 @@ class DeepKernelSUQ:
 
         return posterior_nlls.to(self.device)
 
-    def train_diagonal(self, num_epochs=20, batch_size=16, print_every=1, learning_rate=0.0005, weight_decay=1e-5):
+    def train_diagonal(self, num_epochs=20, batch_size=20, print_every=1, learning_rate=0.0005, weight_decay=1e-5):
         train_x = torch.cat((self.point_cloud, self.partial_cloud), 1).to(self.device)
         num_batches = np.ceil(train_x.size(0) / batch_size).astype('int')
         optimizer = torch.optim.Adam([
@@ -182,10 +184,10 @@ class DeepKernelSUQ:
             for j in range(num_batches):
                 if j < num_batches-1:
                     x = train_x[j*batch_size: (j+1)*batch_size]
-                    y = self.partial_value_train[j*batch_size: (j+1)*batch_size]
+                    y = self.partial_value_train[j*batch_size: (j+1)*batch_size].to(self.device)
                 else:
                     x = train_x[(num_batches-1)*batch_size:]
-                    y = self.partial_value_train[(num_batches-1)*batch_size:]
+                    y = self.partial_value_train[(num_batches-1)*batch_size:].to(self.device)
                 optimizer.zero_grad()
                 output = self.get_posterior_diagonal(x, y)
                 # print(output)
@@ -210,10 +212,10 @@ class DeepKernelSUQ:
             for j in range(num_batches):
                 if j < num_batches - 1:
                     x = train_x[j * batch_size: (j + 1) * batch_size]
-                    y = self.partial_value_train[j * batch_size: (j + 1) * batch_size]
+                    y = self.partial_value_train[j * batch_size: (j + 1) * batch_size].to(self.device)
                 else:
                     x = train_x[(num_batches - 1) * batch_size:]
-                    y = self.partial_value_train[(num_batches - 1) * batch_size:]
+                    y = self.partial_value_train[(num_batches - 1) * batch_size:].to(self.device)
                 optimizer.zero_grad()
                 output = self.get_posterior_cholesky(x, y)
                 # print(output)
@@ -247,22 +249,22 @@ class DeepKernelSUQ:
         for i in range(bs):
             # print(f'cloud {i}')
             mapped_cloud = self.cov_network(encoded_points[i])
-            covar_module = gpytorch.kernels.ScaleKernel(gpytorch.kernels.RBFKernel(ard_num_dims=mapped_cloud.size(1)))
-            cov_matrix = covar_module(mapped_cloud).evaluate_kernel().to_dense()
+            covar_module = gpytorch.kernels.ScaleKernel(
+                gpytorch.kernels.RBFKernel(ard_num_dims=mapped_cloud.size(1))).to(self.device)
+            cov_matrix = covar_module(mapped_cloud).evaluate_kernel().to_dense().to(self.device)
             kernel_ff = cov_matrix[:points_to_predict.size(0), :points_to_predict.size(0)]
             kernel_pf = cov_matrix[points_to_predict.size(0):, :points_to_predict.size(0)]
             kernel_pp = cov_matrix[points_to_predict.size(0):, points_to_predict.size(0):]
-            additional_noise = self.noise_var * torch.eye(partial.shape[1])
+            additional_noise = self.noise_var * torch.eye(partial.shape[1]).to(self.device)
             kernel_with_noise = (kernel_pp + additional_noise).to(self.device)
-            posterior_mean = kernel_pf.T @ torch.linalg.inv(kernel_with_noise) @ self.partial_value_test[i]
+            posterior_mean = kernel_pf.T @ torch.linalg.inv(kernel_with_noise) @ self.partial_value_test[i].to(
+                self.device)
             posterior_var = kernel_ff - kernel_pf.T @ torch.linalg.inv(kernel_with_noise) @ kernel_pf
             posterior_diag = torch.diagonal(posterior_var, 0)
-            # posterior_nlls[i] = 0.5 * (
-            #            torch.log(torch.linalg.det(posterior_diag) + 1e-6) - torch.log(torch.tensor(1e-6))
-            #            + posterior_mean.T @ torch.linalg.inv(posterior_diag) @ posterior_mean)
+
             with torch.no_grad():
                 prob_on_surface = norm.pdf(np.zeros(posterior_mean.shape), loc=posterior_mean.cpu().detach().numpy(),
-                                           scale=np.sqrt(posterior_diag).cpu().detach().numpy())
+                                           scale=np.sqrt(posterior_diag.cpu().detach().numpy()))
             gp = points_to_predict.cpu().numpy()
             gp_x = gp[:, 0].reshape(self.grid_sizes)
             gp_y = gp[:, 1].reshape(self.grid_sizes)
