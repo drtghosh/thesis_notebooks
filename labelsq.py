20527c8bdac184a88dcc5532be7c5a4c0fe3c26e
Debabrata Ghosh <rijugh@gmail.com>
2024-09-20 16:30:58 +0200

    new avenue

--- labelsq.py
+++ labelsq.py
@@ -3,7 +3,7 @@ import torch
 import gpytorch
 import numpy as np
 from scipy.stats import norm
-from torch.distributions import MultivariateNormal as multiNorm
+# from torch.distributions import MultivariateNormal as multiNorm
 import matplotlib.pyplot as plt
 # import torch.nn as nn
 # import time
@@ -20,7 +20,7 @@ from models import MLPGrow, PointNetEncoder
 # import multiprocessing
 
 
-class DeepKernelSUQ:
+class LabeledSUQ:
     """
     Class of Neural Shape Uncertainty Quantification conditioned on a given incomplete point cloud
     by minimizing the -ve log likelihood of the complete point cloud data estimate from the posterior
@@ -185,101 +185,6 @@ class DeepKernelSUQ:
 
         return posterior_nlls.to(self.device)
 
-    def get_posterior_diagonal(self, x, y):
-        partial = x[:, self.point_cloud.shape[1]:, :]
-        partial = partial.to(self.device)
-        # compute encoding in a batch
-        encoding = self.encoder(partial.transpose(1, 2))
-        # repeat encoding for each point in full point cloud
-        encoded_x = torch.cat((x, encoding.unsqueeze(1).repeat(1, x.size(1), 1)), 2)
-        # collect batch size
-        bs = x.size(0)
-        # create empty list to store negative log-likelihoods of multivariate normals
-        posterior_nlls = torch.empty(bs).to(self.device)
-        for i in range(bs):
-            mapped_cloud = self.map_norm(self.cov_network(encoded_x[i]))
-            covar_module = gpytorch.kernels.RBFKernel(ard_num_dims=mapped_cloud.size(1)).to(self.device)
-            cov_matrix = covar_module(mapped_cloud).evaluate_kernel().to_dense().to(self.device)
-            kernel_ff = cov_matrix[:self.point_cloud.shape[1], :self.point_cloud.shape[1]]
-            kernel_pf = cov_matrix[self.point_cloud.shape[1]:, :self.point_cloud.shape[1]]
-            kernel_pp = cov_matrix[self.point_cloud.shape[1]:, self.point_cloud.shape[1]:]
-            additional_noise = self.noise_var * torch.eye(self.partial_cloud.shape[1]).to(self.device)
-            kernel_with_noise = (kernel_pp + additional_noise).to(self.device)
-            posterior_mean = kernel_pf.T @ torch.linalg.inv(kernel_with_noise) @ y[i]
-            posterior_var = kernel_ff - kernel_pf.T @ torch.linalg.inv(kernel_with_noise) @ kernel_pf
-            posterior_diag = torch.diag(torch.diagonal(posterior_var, 0))
-            posterior_nlls[i] = 0.5 * (
-                        torch.log(torch.linalg.det(posterior_diag) + 1e-6) - torch.log(torch.tensor(1e-6))
-                        + posterior_mean.T @ torch.linalg.inv(posterior_diag) @ posterior_mean)
-
-        return posterior_nlls.to(self.device)
-
-    def get_posterior_cholesky(self, x, y):
-        partial = x[:, self.point_cloud.shape[1]:, :]
-        partial = partial.to(self.device)
-        # compute encoding in a batch
-        encoding = self.encoder(partial.transpose(1, 2))
-        # repeat encoding for each point in full point cloud
-        encoded_x = torch.cat((x, encoding.unsqueeze(1).repeat(1, x.size(1), 1)), 2)
-        print(encoded_x)
-        # collect batch size
-        bs = x.size(0)
-        # create empty list to store negative log-likelihoods of multivariate normals
-        posterior_nlls = torch.empty(bs).to(self.device)
-        for i in range(bs):
-            mapped_cloud = self.map_norm(self.cov_network(encoded_x[i]))
-            covar_module = gpytorch.kernels.ScaleKernel(
-                gpytorch.kernels.RBFKernel(ard_num_dims=mapped_cloud.size(1))).to(self.device)
-            cov_matrix = covar_module(mapped_cloud).evaluate_kernel().to_dense().to(self.device)
-            print(cov_matrix)
-            kernel_ff = cov_matrix[:self.point_cloud.shape[1], :self.point_cloud.shape[1]]
-            kernel_pf = cov_matrix[self.point_cloud.shape[1]:, :self.point_cloud.shape[1]]
-            kernel_pp = cov_matrix[self.point_cloud.shape[1]:, self.point_cloud.shape[1]:]
-            additional_noise = self.noise_var * torch.eye(self.partial_cloud.shape[1]).to(self.device)
-            kernel_with_noise = (kernel_pp + additional_noise).to(self.device)
-            posterior_mean = kernel_pf.T @ torch.linalg.inv(kernel_with_noise) @ y[i]
-            posterior_var = kernel_ff - kernel_pf.T @ torch.linalg.inv(kernel_with_noise) @ kernel_pf
-            posterior_cholesky_upper, info = torch.linalg.cholesky_ex(posterior_var, upper=True)
-            posterior_cholesky = posterior_cholesky_upper.T @ posterior_cholesky_upper
-            posterior_nlls[i] = 0.5 * (
-                        torch.log(torch.linalg.det(posterior_cholesky) + 1e-6) - torch.log(torch.tensor(1e-6)) +
-                        posterior_mean.T @ torch.linalg.inv(posterior_cholesky) @ posterior_mean)
-
-        return posterior_nlls.to(self.device)
-
-    def get_posterior_cholesky_plus_noise(self, x, y):
-        partial = x[:, self.point_cloud.shape[1]:, :]
-        partial = partial.to(self.device)
-        # compute encoding in a batch
-        encoding = self.encoder(partial.transpose(1, 2))
-        # repeat encoding for each point in full point cloud
-        encoded_x = torch.cat((x, encoding.unsqueeze(1).repeat(1, x.size(1), 1)), 2)
-        # collect batch size
-        bs = x.size(0)
-        # create empty list to store negative log-likelihoods of multivariate normals
-        posterior_nlls = torch.empty(bs).to(self.device)
-        for i in range(bs):
-            mapped_cloud = self.cov_network(encoded_x[i])
-            covar_module = gpytorch.kernels.ScaleKernel(
-                gpytorch.kernels.RBFKernel(ard_num_dims=mapped_cloud.size(1))).to(self.device)
-            cov_matrix = covar_module(mapped_cloud).evaluate_kernel().to_dense().to(self.device)
-            kernel_ff = cov_matrix[:self.point_cloud.shape[1], :self.point_cloud.shape[1]]
-            kernel_pf = cov_matrix[self.point_cloud.shape[1]:, :self.point_cloud.shape[1]]
-            kernel_pp = cov_matrix[self.point_cloud.shape[1]:, self.point_cloud.shape[1]:]
-            additional_noise = self.noise_var * torch.eye(self.partial_cloud.shape[1]).to(self.device)
-            kernel_with_noise = (kernel_pp + additional_noise).to(self.device)
-            posterior_mean = kernel_pf.T @ torch.linalg.inv(kernel_with_noise) @ y[i]
-            posterior_var = kernel_ff - kernel_pf.T @ torch.linalg.inv(kernel_with_noise) @ kernel_pf
-            posterior_cholesky_upper, info = torch.linalg.cholesky_ex(posterior_var, upper=True)
-            posterior_cholesky_plus_noise = (
-                        (posterior_cholesky_upper.T @ posterior_cholesky_upper) + self.noise_var * torch.eye(
-                            posterior_cholesky_upper.shape[0])).to(self.device)
-            posterior_nlls[i] = 0.5 * (
-                    torch.log(torch.linalg.det(posterior_cholesky_plus_noise) + 1e-6) - torch.log(torch.tensor(1e-6)) +
-                    posterior_mean.T @ torch.linalg.inv(posterior_cholesky_plus_noise) @ posterior_mean)
-
-        return posterior_nlls.to(self.device)
-
     def train_without_mapping(self, num_epochs=20, batch_size=16, print_every=1, learning_rate=0.0005, weight_decay=1e-5, loss_type=None):
         train_x = torch.cat((self.point_cloud, self.partial_cloud), 1).to(self.device)
         num_batches = np.ceil(train_x.size(0) / batch_size).astype('int')
@@ -327,90 +232,6 @@ class DeepKernelSUQ:
                     y = self.partial_value_train[(num_batches-1)*batch_size:].to(self.device)
                 optimizer.zero_grad()
                 output = self.get_posterior_with_mapping(x, y)
-                # print(output)
-                loss = torch.mean(output)
-                loss.backward()
-                optimizer.step()
-                training_loss += loss.item()
-            training_loss /= num_batches
-            if i % print_every == 0:
-                print(f"Epoch:{i}, Loss: {training_loss}")
-
-    def train_diagonal(self, num_epochs=20, batch_size=16, print_every=1, learning_rate=0.0005, weight_decay=1e-5):
-        train_x = torch.cat((self.point_cloud, self.partial_cloud), 1).to(self.device)
-        num_batches = np.ceil(train_x.size(0) / batch_size).astype('int')
-        optimizer = torch.optim.AdamW([
-            {'params': self.encoder.parameters()},
-            {'params': self.cov_network.parameters()}
-        ], learning_rate, weight_decay=weight_decay)
-
-        training_loss = 0
-        for i in range(num_epochs):
-            for j in range(num_batches):
-                if j < num_batches-1:
-                    x = train_x[j*batch_size: (j+1)*batch_size]
-                    y = self.partial_value_train[j*batch_size: (j+1)*batch_size].to(self.device)
-                else:
-                    x = train_x[(num_batches-1)*batch_size:]
-                    y = self.partial_value_train[(num_batches-1)*batch_size:].to(self.device)
-                optimizer.zero_grad()
-                output = self.get_posterior_diagonal(x, y)
-                # print(output)
-                loss = torch.mean(output)
-                loss.backward()
-                optimizer.step()
-                training_loss += loss.item()
-            training_loss /= num_batches
-            if i % print_every == 0:
-                print(f"Epoch:{i}, Loss: {training_loss}")
-
-    def train_cholesky(self, num_epochs=20, batch_size=16, print_every=1, learning_rate=0.0005, weight_decay=1e-5):
-        train_x = torch.cat((self.point_cloud, self.partial_cloud), 1).to(self.device)
-        num_batches = np.ceil(train_x.size(0) / batch_size).astype('int')
-        optimizer = torch.optim.AdamW([
-            {'params': self.encoder.parameters()},
-            {'params': self.cov_network.parameters()}
-        ], learning_rate, weight_decay=weight_decay)
-
-        training_loss = 0
-        for i in range(num_epochs):
-            for j in range(num_batches):
-                if j < num_batches - 1:
-                    x = train_x[j * batch_size: (j + 1) * batch_size]
-                    y = self.partial_value_train[j * batch_size: (j + 1) * batch_size].to(self.device)
-                else:
-                    x = train_x[(num_batches - 1) * batch_size:]
-                    y = self.partial_value_train[(num_batches - 1) * batch_size:].to(self.device)
-                optimizer.zero_grad()
-                output = self.get_posterior_cholesky(x, y)
-                # print(output)
-                loss = torch.mean(output)
-                loss.backward()
-                optimizer.step()
-                training_loss += loss.item()
-            training_loss /= num_batches
-            if i % print_every == 0:
-                print(f"Epoch:{i}, Loss: {training_loss}")
-
-    def train_cholesky_plus_noise(self, num_epochs=20, batch_size=16, print_every=1, learning_rate=0.0005, weight_decay=1e-5):
-        train_x = torch.cat((self.point_cloud, self.partial_cloud), 1).to(self.device)
-        num_batches = np.ceil(train_x.size(0) / batch_size).astype('int')
-        optimizer = torch.optim.AdamW([
-            {'params': self.encoder.parameters()},
-            {'params': self.cov_network.parameters()}
-        ], learning_rate, weight_decay=weight_decay)
-
-        training_loss = 0
-        for i in range(num_epochs):
-            for j in range(num_batches):
-                if j < num_batches - 1:
-                    x = train_x[j * batch_size: (j + 1) * batch_size]
-                    y = self.partial_value_train[j * batch_size: (j + 1) * batch_size].to(self.device)
-                else:
-                    x = train_x[(num_batches - 1) * batch_size:]
-                    y = self.partial_value_train[(num_batches - 1) * batch_size:].to(self.device)
-                optimizer.zero_grad()
-                output = self.get_posterior_cholesky_plus_noise(x, y)
                 # print(output)
                 loss = torch.mean(output)
                 loss.backward()
