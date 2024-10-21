d67d18d675adf7a412f9966b58ccd68d1b604bc4
Debabrata Ghosh <rijugh@gmail.com>
2024-10-21 17:13:33 +0200

    new datset creation

---
+++ create_data_dgcn_cc.py
@@ -0,0 +1,100 @@
+import random
+
+import numpy
+import torch
+import numpy as np
+from scipy.spatial import distance
+
+
+class Circ:
+    def __init__(self, circle_center_x, rec_long_side, rec_short_side, seed):
+        self.circle_center_x = circle_center_x
+        self.rec_long_side = rec_long_side
+        self.rec_short_side = rec_short_side
+        self.circle_radius = distance.euclidean([circle_center_x, 0], [rec_long_side / 2, rec_short_side / 2])
+        self.seed = seed
+
+    def create_samples(self, points_per_unit=100):
+        diff_x = self.circle_center_x - (self.rec_long_side / 2)
+        assert diff_x > 0
+
+        add_angle = 1 / (points_per_unit * self.circle_radius)
+        full_circles_perimeter = 4 * np.pi * self.circle_radius
+        eye_samples = torch.empty((int(points_per_unit * full_circles_perimeter), 2), dtype=torch.float32)
+
+        full_circle_sample = int(full_circles_perimeter * points_per_unit / 2)
+
+        for i in range(full_circle_sample):
+            eye_samples[i] = torch.Tensor(
+                [-self.circle_center_x + self.circle_radius * np.cos(i * add_angle),
+                 self.circle_radius * np.sin(i * add_angle)])
+            eye_samples[full_circle_sample + i] = torch.Tensor(
+                [self.circle_center_x + self.circle_radius * np.cos(i * add_angle),
+                 self.circle_radius * np.sin(i * add_angle)])
+
+        return eye_samples
+
+    def subsample_shape(self, points_per_unit, percent_to_keep, special_selection=False):
+        eye_samples = self.create_samples(points_per_unit)
+        (dm, dn) = eye_samples.shape
+        num_eye_subsample = int(percent_to_keep*dm)
+
+        rand_eye_idx = torch.randperm(dm)
+        if special_selection:
+            left_sample_start = int((dm - num_eye_subsample) / 4)
+            left_sample_end = int((dm + num_eye_subsample) / 4)
+            left_sample_range = np.arange(left_sample_start, left_sample_end)
+            right_sample1_start = int(dm / 2)
+            right_sample1_end = int(dm / 2 + num_eye_subsample / 4)
+            right_sample1_range = np.arange(right_sample1_start, right_sample1_end)
+            right_sample2_start = int(dm - num_eye_subsample / 4)
+            right_sample2_end = dm
+            right_sample2_range = np.arange(right_sample2_start, right_sample2_end)
+            right_sample_range = np.concatenate((right_sample1_range, right_sample2_range))
+            eye_subsample_indices = np.concatenate((left_sample_range, right_sample_range))
+            eye_remaining_indices = np.setdiff1d(np.arange(dm), eye_subsample_indices)
+        else:
+            eye_subsample_indices = rand_eye_idx[:num_eye_subsample]
+            eye_remaining_indices = rand_eye_idx[num_eye_subsample:]
+
+        return eye_samples[eye_remaining_indices, :], eye_samples[eye_subsample_indices, :]
+
+    def create_dataset(self, train_instances=1000, test_instances=1, ppu_choices=None, percent_choices=None,
+                       special_entry=10):
+        full_clouds_positive = []
+        partial_clouds = []
+        labels = []
+        partial_clouds_test = []
+        labels_test = []
+        for i in range(train_instances):
+            if ppu_choices is not None:
+                ppu = random.choice(ppu_choices)
+            else:
+                ppu = random.choice([10, 25, 50, 100, 150, 200])
+            if percent_choices is not None:
+                percent = random.choice(percent_choices)
+            else:
+                percent = random.choice([0.05, 0.1, 0.15, 0.2, 0.25])
+            if i % special_entry == 0:
+                ds, dss = self.subsample_shape(ppu, percent, True)
+            else:
+                ds, dss = self.subsample_shape(ppu, percent)
+            full_clouds_positive.append(ds.numpy())
+            labels.append(np.array([1, 0]))
+            partial_clouds.append(dss.numpy())
+
+        for j in range(test_instances):
+            if ppu_choices is not None:
+                ppu = random.choice(ppu_choices)
+            else:
+                ppu = random.choice([10, 25, 50, 100, 150, 200])
+            if percent_choices is not None:
+                percent = random.choice(percent_choices)
+            else:
+                percent = random.choice([0.05, 0.1, 0.15, 0.2, 0.25])
+            ds, dss = self.subsample_shape(ppu, percent, True)
+            partial_clouds_test.append(dss.numpy())
+            labels_test.append(np.array([1, 0]))
+
+        return np.array(full_clouds_positive), np.array(partial_clouds), np.array(labels), np.array(
+            partial_clouds_test), np.array(labels_test)
--- dgcnsuq.py
+++ dgcnsuq.py
@@ -5,6 +5,14 @@ import numpy as np
 from models import DeepGCN, PointNetEncoder
 
 
+def compute_log_likelihood(data, label, mean, variance):
+    dist_term = -0.5 * (label - mean).T @ torch.linalg.inv(variance) @ (label - mean)
+    det_term = -0.5 * torch.log(torch.linalg.det(variance))
+    const_term = -0.5 * data.size(0) * torch.log(2 * torch.tensor(torch.pi))
+    log_likelihood = dist_term + det_term + const_term
+    return log_likelihood
+
+
 class DGCN:
     """
     Class of Deep Gaussian Covariance Network intended to learn non-stationary hyperparameters
@@ -30,46 +38,43 @@ class DGCN:
         self.device = torch.device("cuda" if use_cuda else "cpu")
 
         # initialize the Deep GCN network
-        self.kernel_net = DeepGCN(32, 2, self.space_dim, 2)
+        self.kernel_net = DeepGCN(32, 2, self.space_dim, 3)
         self.kernel_net.to(self.device)
 
-    def compute_kernel(self, x, y):
+    def compute_kernel(self, x):
         scale_matrix = self.kernel_net(x)
         scaled_data = scale_matrix * x[..., None]
-        kernel_matrix = torch.exp(-0.5 * torch.cdist(scaled_data, scaled_data)).mean(dim=0)
-
-        return kernel_matrix, scaled_data
+        reshaped_data = torch.permute(scaled_data, (2, 0, 1))
+        kernel_matrix = torch.exp(-0.5 * torch.cdist(reshaped_data, reshaped_data)).mean(dim=0)
+        return kernel_matrix, reshaped_data
 
     def compute_log_marginal(self, x, y):
-        covar_matrix, scaled_data = self.compute_kernel(x, y)
+        covar_matrix, reshaped_data = self.compute_kernel(x)
         noise = self.noise_variance * torch.eye(x.size(0)).to(self.device)
         covar_with_noise = covar_matrix + noise
-        dist_term = -0.5 * y.T @ torch.linalg.inv(covar_with_noise) @ y
-        det_term = -0.5 * torch.log(torch.linalg.det(covar_with_noise))
-        const_term = -0.5 * x.size(0) * torch.log(2 * torch.Tensor(torch.pi))
-        log_likelihood = dist_term + det_term + const_term
+        log_likelihood = compute_log_likelihood(x, y, 0, covar_with_noise)
         return log_likelihood
 
     def compute_posterior_batch(self, train_x, train_y, test_x, test_y, evaluate=False):
-        covar_matrix_train, scaled_data_train = self.compute_kernel(train_x, train_y)
+        covar_matrix_train, reshaped_data_train = self.compute_kernel(train_x)
         noise = self.noise_variance * torch.eye(train_x.size(0)).to(self.device)
         covar_with_noise = covar_matrix_train + noise
         if evaluate:
             self.kernel_net.eval()
         scale_matrix_test = self.kernel_net(test_x)
         scaled_data_test = scale_matrix_test * test_x[..., None]
-        covar_matrix_test = torch.exp(-0.5 * torch.cdist(scaled_data_test, scaled_data_test)).mean(dim=0)
-        covar_matrix_mixed = torch.exp(-0.5 * torch.cdist(scaled_data_test, scaled_data_train)).mean(dim=0)
+        reshaped_data_test = torch.permute(scaled_data_test, (2, 0, 1))
+        covar_matrix_test = torch.exp(-0.5 * torch.cdist(reshaped_data_test, reshaped_data_test)).mean(dim=0)
+        covar_matrix_mixed = torch.exp(-0.5 * torch.cdist(reshaped_data_test, reshaped_data_train)).mean(dim=0)
         covar_inv = torch.linalg.inv(covar_with_noise)
         posterior_mean = covar_matrix_mixed @ covar_inv @ train_y
         posterior_var = covar_matrix_test - covar_matrix_mixed @ covar_inv @ covar_matrix_mixed.T
-        dist_term = -0.5 * (test_y - posterior_mean).T @ torch.linalg.inv(posterior_var) @ (test_y - posterior_mean)
-        det_term = -0.5 * torch.log(torch.linalg.det(posterior_var))
-        const_term = -0.5 * test_x.size(0) * torch.log(2 * torch.Tensor(torch.pi))
-        log_likelihood = dist_term + det_term + const_term
-        return log_likelihood
+        # log_likelihood = compute_log_likelihood(test_x, test_y, posterior_mean, posterior_var)
+        # return log_likelihood
+        return posterior_mean, posterior_var
 
-    def train_kernel(self, num_epochs=20, batch_size=20, learning_rate=0.001, weight_decay=1e-5, use_posterior=False):
+    def train_kernel(self, num_epochs=20, batch_size=20, learning_rate=0.001, weight_decay=1e-5, print_every=2,
+                     use_posterior=False):
         train_x = self.training_data.to(self.device)
         train_y = self.training_label.to(self.device)
         num_batches = np.ceil(train_x.size(0) / batch_size).astype('int')
@@ -86,4 +91,15 @@ class DGCN:
                     x = train_x[(num_batches - 1) * batch_size:]
                     y = train_y[(num_batches - 1) * batch_size:]
                 optimizer.zero_grad()
-
+                if use_posterior:
+                    output = -self.compute_posterior_batch(x, y, x, y)
+                    print(output)
+                else:
+                    output = -self.compute_log_marginal(x, y)
+                    loss = torch.mean(output)
+                    loss.backward()
+                    optimizer.step()
+                    training_loss += loss.item()
+            training_loss /= num_batches
+            if i % print_every == 0:
+                print(f"Epoch:{i}, Loss: {training_loss}")
